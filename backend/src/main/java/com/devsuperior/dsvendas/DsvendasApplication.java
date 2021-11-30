3fe522b27bd7dc4a89dcf04eed8d6fe710103f21
João Paulo <jptick@gmail.com>
2021-11-29 21:44:52 -0300

    Update To Deploy in Tomcat Locaweb

--- backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
+++ backend/src/main/java/com/devsuperior/dsvendas/DsvendasApplication.java
@@ -2,9 +2,10 @@ package com.devsuperior.dsvendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DsvendasApplication {
+public class DsvendasApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DsvendasApplication.class, args);
